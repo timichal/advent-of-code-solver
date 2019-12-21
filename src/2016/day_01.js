ca5e6c3cc5c34feb5f88e5002b9156d1f9c25a87
Michal Zlatkovský <michal@zlatkovsky.cz>
2019-12-21 12:16:41 +0100

    2016-01 redux

--- src/2016/day_01.js
+++ src/2016/day_01.js
@@ -1,26 +1,29 @@
 export const sixteenOne = (input) => {
-  const instructions = input[0].split(", ");
-  let [x, y, direction] = [0, 0, 0];
-  let duplicate;
-  const visitedLocs = [];
-  // direction: 0 north, 1 east, 2 south, 3 west
-  instructions.forEach((instruction) => {
-    if (instruction[0] === "R") direction = (direction + 1) % 4;
-    else direction = direction === 0 ? 3 : direction - 1;
+  const instructions = input[0]
+    .split(", ")
+    .join("")
+    .replace(/\d+/g, (match) => "M".repeat(match));
 
-    const steps = Number(instruction.substring(1));
-    Array.from(Array(steps)).forEach(() => {
-      if (direction === 0) x += 1;
-      else if (direction === 1) y += 1;
-      else if (direction === 2) x -= 1;
-      else if (direction === 3) y -= 1;
+  const visitedLocs = [];
+  let duplicate;
+  // direction: 0 north, 1 east, 2 south, 3 west
+  const planner = [...instructions].reduce(([x, y, direction], instruction) => {
+    if (instruction === "R") return [x, y, (direction + 1) % 4];
+    if (instruction === "L") return [x, y, direction === 0 ? 3 : direction - 1];
+    if (instruction === "M") {
       const duplicates = visitedLocs.filter((loc) => loc[0] === x && loc[1] === y);
       if (!duplicate && duplicates.length > 0) [duplicate] = duplicates;
       if (!duplicate) visitedLocs.push([x, y]);
-    });
-  });
 
-  const part1 = Math.abs(x) + Math.abs(y);
+      if (direction === 0) return [x + 1, y, direction];
+      if (direction === 1) return [x, y + 1, direction];
+      if (direction === 2) return [x - 1, y, direction];
+      if (direction === 3) return [x, y - 1, direction];
+    }
+    return [x, y, direction];
+  }, [0, 0, 0]);
+
+  const part1 = Math.abs(planner[0]) + Math.abs(planner[1]);
   const part2 = Math.abs(duplicate[0]) + Math.abs(duplicate[1]);
   return [part1, part2];
 };
