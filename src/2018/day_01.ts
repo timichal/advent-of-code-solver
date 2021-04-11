4424c2cbbda92cd6623ec526165b21aa73c7c1e2
Michal Zlatkovský <michal@zlatkovsky.cz>
2021-04-11 12:30:19 +0200

    2018/01 set

--- src/2018/day_01.ts
+++ src/2018/day_01.ts
@@ -6,14 +6,15 @@ const eighteenOne = (input: string[]): [number, number] => {
   const part1 = sum(instructions);
 
   let part2 = 0;
-  const frequencies: number[] = [];
+  const frequencies = new Set();
+  let lastFrequency = 0;
   while (!part2) {
-    instructions.forEach((instruction) => {
-      const lastFrequency = frequencies.slice(-1)[0];
-      const newFrequency = (lastFrequency ?? 0) + instruction;
-      if (frequencies.includes(newFrequency) && !part2) part2 = newFrequency;
-      else frequencies.push(newFrequency);
-    });
+    for (let i = 0; i < instructions.length; i++) {
+      const newFrequency = (lastFrequency ?? 0) + instructions[i];
+      if (frequencies.has(newFrequency) && !part2) part2 = newFrequency;
+      else frequencies.add(newFrequency);
+      lastFrequency = newFrequency;
+    }
   }
   return [part1, part2];
 };
